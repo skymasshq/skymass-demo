0581d3b67d48468b51820366d7e90c132df52f13
Pasha Sadri <psadri@gmail.com>
2022-09-24 21:23:45 -0700

    Supabase Todo List

--- supabase_todolist.mjs
+++ supabase_todolist.mjs
@@ -1,65 +1,48 @@
 import { SkyMass } from "@skymass/skymass";
-import admin from "firebase-admin";
+import { createClient } from "@supabase/supabase-js";
 
 // NOTE: this entire script runs exclusively on the server.  The UI is provided by SkyMass.
 
-// initalize Firebase...
-const firebase = admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(process.env["FIREBASE_CERT_JSON"])
-  ),
-  databaseURL: process.env["FIREBASE_RTDB_URL"],
-});
+// Create a single Supabase client for interacting with your database
+const supabase = createClient(
+  process.env["SUPABASE_URL"],
+  process.env["SUPABASE_KEY"]
+);
 
 // create a SkyMass instance
 const sm = new SkyMass({ key: process.env["SKYMASS_KEY"] });
 
 // register the application
-sm.page("/firebase-todolist", async (ui) => {
+sm.page("/supabase-todolist", async (ui) => {
   // This function is run on first app access and every time something
   // significant happens in the UI (for example when a button is pressed,
   // a table row is selected, ...)
 
   // SkyMass uses Markdown to emit and format text.
   // Here we emit a level 1 (#) header
-  ui.md`# ☑️ Firebase Todo List`;
+  ui.md`# ☑️ Supabase Todo List`;
 
   // This app is configured at skymass.dev to require a login
   // so user will always be set to { email: ... }
   const { email } = ui.user();
 
-  // A checkbox input to control whether we include completed todos
-  // It's .val is used below to filter the done todos.
+  // A checkbox input to control whether we hide completed todos
   const hideDone = ui.boolean("hide_done", { label: "Hide Completed Todos" });
 
-  // Firebase Real Time DB let's us subscribe to update events.
-  // Let's create a subscription using ui.subscribe()
-  // Everytime the "update" function is called, UserTodoList is
-  // updated to the latest value.  UserTodoList is used later
-  // in the code to populate a table that lists the todos.
-  const UserTodoList = ui.subscribe(
-    "todos_subscription",
-    (update) => {
-      const userTodos = dbRef(email);
-      // Firebase gives us an object or null so we need a function to
-      // convert it into an array of todos to render as rows in a table.
-      const handler = (snapshot) => {
-        const list = convertSnapShotToList(snapshot);
-        // if hideDone is true, filter out the done todos
-        const filtered = hideDone.val
-          ? list.filter((todo) => !todo.done)
-          : list;
-        // call update with the latest list
-        update(list);
-      };
-      // register the handler
-      userTodos.on("value", handler);
-      // return a cleanup function
-      return () => userTodos.off("value", handler);
-    },
-    // our function uses hideDone, so we specify it as a dependency
-    [hideDone]
-  );
+  // when hideDone is checked, doneFilter filters for todos whose done = false
+  const doneFilter = hideDone.val ? { done: false } : {};
+
+  // SELECT user's todos from the database. supabase.select returns a Promise.
+  const todosPromise = supabase
+    .from("todo")
+    .select("id,title,due_by,done")
+    .match({ created_by: email, ...doneFilter })
+    .then(({ data }) => {
+      data.forEach((todo) => {
+        todo.due_by = new Date(todo.due_by);
+      });
+      return data;
+    });
 
   // SkyMass Markdown supports mentioning widgets using {widget_id} to
   // control their placement. {widgets} on the same line are laid out in
@@ -69,10 +52,12 @@ sm.page("/firebase-todolist", async (ui) => {
   // which means todos will occupy 1/3 of the available row width.
   ui.md`{todos} ~ ~`;
 
-  // Render table with the latest todos contained in UserTodoList
-  // that we set up earlier with ui.subscribe
-  const table = ui.table("todos", UserTodoList, {
-    loading: "Loading Todos from Firebase",
+  // Render table with the items returned from the query
+  // Note that 'todosPromise', which contains the list of todos is a Promise.
+  // ui.table automatically handles literals or Promises.
+  // The optional columns prop specifies col rendering options.
+  const table = ui.table("todos", todosPromise, {
+    loading: "Loading Todos...",
     empty: "No Pending Todos.  Use 'New Todo' to add some.",
     columns: {
       "*": { search: false },
@@ -89,14 +74,19 @@ sm.page("/firebase-todolist", async (ui) => {
   const [todo] = table.selection;
 
   // render a button to mark the selected todo as done/todo.
+  // button is disabled unless a table row is selected
   const toggle = ui.button("toggle", {
     label: todo ? (todo.done ? "Mark as Todo" : "Mark as Done") : "Toggle",
     disabled: !todo,
   });
   // .didClick toggles true when the button is clicked causing
-  // this code block to be entered once per button click
+  // this code block to be entered on button clicks
   if (toggle.didClick) {
-    await dbRef(email, todo.id).update({ done: !todo.done });
+    // note: we can await async operations
+    await supabase
+      .from("todo")
+      .update({ done: !todo.done })
+      .match({ id: todo.id });
     ui.toast(todo.done ? "Marked as todo" : "Marked as done");
   }
 
@@ -107,8 +97,9 @@ sm.page("/firebase-todolist", async (ui) => {
   });
   if (del.didClick) {
     // modal interactions, like ui.confirm, return Promises which we await!
+    // ui.confirm returns a Promise that resolves to true / false
     if (await ui.confirm({ text: "Are you sure?" })) {
-      dbRef(email, todo.id).remove();
+      await supabase.from("todo").delete().match({ id: todo.id });
       ui.toast("Deleted");
     }
   }
@@ -117,16 +108,18 @@ sm.page("/firebase-todolist", async (ui) => {
   if (add.didClick) {
     // Similar to ui.confirm, ui.modal returns a Promise that we await
     // until the modal is closed. Modals accept a function that controls
-    //  what happens inside of them, similar to the top level app. This
+    // what happens inside of them, similar to the top level app. This
     // function runs independently while the parent function is 'awaiting'
     // the completion of the modal.
     await ui.modal("add", async (ui) => {
       ui.md`#### New Todo`;
 
+      // default due by date
       const tomorrow = new Date();
       tomorrow.setDate(tomorrow.getDate() + 1);
 
       // forms are used to group related inputs together.
+      // here we have two fields: title and due_by and the 'Add' button.
       const newTodo = ui.form("todo", {
         fields: {
           title: ui.string("title", { placeholder: "Title", required: true }),
@@ -142,12 +135,9 @@ sm.page("/firebase-todolist", async (ui) => {
       // similar to button .didClick, .didSubmit toggles true when the form is submitted
       // newTodo.val will contain { title: ..., due_by: ...}
       if (newTodo.didSubmit) {
-        const { title, due_by } = newTodo.val;
-        await dbRef(email).push().set({
-          title,
-          due_by: due_by.toISOString(), // store dates as a serializable format
-          done: false,
-        });
+        await supabase
+          .from("todo")
+          .insert([{ ...newTodo.val, created_by: email }]);
         ui.toast("Added new todo");
         ui.close();
       }
@@ -155,24 +145,5 @@ sm.page("/firebase-todolist", async (ui) => {
   }
 
   // link to this file
-  ui.md`[View Source on GitHub](https://github.com/skymasshq/skymass-demo/blob/main/firebase_todolist.mjs)`;
+  ui.md`[View Source on GitHub](https://github.com/skymasshq/skymass-demo/blob/main/supabase_todolist.mjs)`;
 });
-
-// utility function to return a firebase reference
-// to the user's todos and optionally a specific todo id
-function dbRef(email, id) {
-  const emailEsc = email.replace(/[.#$/[\]]/g, "_");
-  return firebase.database().ref("todos/" + emailEsc + (id ? "/" + id : ""));
-}
-
-// utility function to convert a Firebase snapshot object to an
-// array of todos suitable for feeding to ui.table
-function convertSnapShotToList(snapshot) {
-  const val = snapshot.val() || {};
-  return Object.keys(val).map((id) => {
-    const todo = val[id];
-    todo.id = id;
-    todo.due_by = new Date(todo.due_by);
-    return todo;
-  });
-}
